2b669fbfa118bfd13fcd5904c719bf231b9d1120
Baihaqi Muhammad <baihaqi.m551@gmail.com>
2024-07-03 21:31:50 +0700

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App.jsx'
 import './index.scss'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./assets/pages/Home/Home.jsx";
-import Login from "./assets/pages/Login/Login.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
 
 const router = createBrowserRouter([
     {
